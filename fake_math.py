d6095ea7798d7cd8062551b6f3bee4db010eca1d
ElviraMirsaitova <amalteya7@gmail.com>
2024-08-20 13:55:10 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(first, second):
+    if second == 0:
+        return(print('Ошибка'))
+
+    else:
+        result_fake = first / second
+        print(result_fake)
+
+divide(10,2)
---
+++ true_math.py
@@ -0,0 +1,11 @@
+import from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return
+
+    else:
+        result_fake = first / second
+        print(result_fake)
+
+divide(10,2)
